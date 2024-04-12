7f3bba5bd4dc022929175c638ee81257e60174af
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-04-11 18:30:00 -0700

    added html layout and basic script

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,34 @@
 const apiKey = '1d59d123b21d759ac3f9305f57e76274';
-const forecast = document.querySelector('.forecast');
+const forecast = document.querySelector('.forecast-row');
 const searchCity = document.querySelector('.searchCity');
 const city = 'Ukiah';
 
 function weatherStats (city){
-    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}$&appid=${apiKey}`;
+    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     
     fetch (queryUrl)
     .then(function(response){
+        if(!response.ok) {
+            throw new Error('Network not responding');
+        }
         return response.json();
-    }).then(function(data){
+    })
+    .then(function(data){
         console.log(data);
+        forecast.innerHTML = JSON.stringify(data);
+    })
+    .catch(function(error) {
+        console.error('There was an issue with the fetch.', error);
     })
 }
 weatherStats(city);
+
+
+function searchWeather() {
+    const city = searchCityInput.value.trim();
+    if (city) {
+        weatherStats(city);
+    } else {
+        console.error('Please enter a city name');
+    }
+}
