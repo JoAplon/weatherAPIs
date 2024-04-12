d859739a065d66df1afe72ca3f5a6ac6163241d0
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-04-11 22:08:56 -0700

    added loop for 5 day forecast and created new element w jscript

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 const apiKey = '1d59d123b21d759ac3f9305f57e76274';
 const forecast = document.querySelector('.forecast-row');
 const searchCity = document.querySelector('.searchCity');
-const city = 'Ukiah';
+const forecastContainer = document.querySelector('.forecast-row');
 
-function weatherStats (city){
-    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function weatherStats (){
+    const queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
     
     fetch (queryUrl)
     .then(function(response){
@@ -15,17 +15,28 @@ function weatherStats (city){
     })
     .then(function(data){
         console.log(data);
-        forecast.innerHTML = JSON.stringify(data);
+        forecastContainer.innerHTML = '';
+        for (let i = 0; i < 5; i++) {
+            const dayData = data.list[i * 8];
+            
+            const createElement = document.createElement('p');
+            createElement.innerHTML = '<strong><br>City Name: </strong>' + city +
+            '<strong><br> Current Weather: </strong>' + dayData.weather[0].main + '<strong><br>The temperature is: </strong>' +
+            dayData.main.temp + '<strong><br>Min Temp: </strong>' + dayData.main.temp_min + '<strong><br>Max Temp: </strong>' + dayData.main.temp_max;
+            
+            forecastContainer.appendChild(createElement);
+            // forecast.innerHTML = JSON.stringify(data);
+        }
     })
     .catch(function(error) {
         console.error('There was an issue with the fetch.', error);
     })
 }
-weatherStats(city);
+weatherStats();
 
 
 function searchWeather() {
-    const city = searchCityInput.value.trim();
+    const city = searchCity.value.trim();
     if (city) {
         weatherStats(city);
     } else {
