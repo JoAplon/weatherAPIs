8a7b5dc89ddaaa04fc5c63c14420d89d38136319
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-04-14 22:14:21 -0700

    receiving api array but not showing up on page yet

--- assets/script.js
+++ assets/script.js
@@ -3,40 +3,79 @@ const forecast = document.querySelector('.forecast-row');
 const searchCity = document.querySelector('.searchCity');
 const forecastContainer = document.querySelector('.forecast-row');
 
-function weatherStats (){
-    const queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
-    
-    fetch (queryUrl)
-    .then(function(response){
-        if(!response.ok) {
-            throw new Error('Network not responding');
-        }
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        forecastContainer.innerHTML = '';
-        for (let i = 0; i < 5; i++) {
-            const dayData = data.list[i * 8];
-            
-            const createElement = document.createElement('p');
-            createElement.innerHTML = '<strong><br>City Name: </strong>' + city +
-            '<strong><br> Current Weather: </strong>' + dayData.weather[0].main + '<strong><br>The temperature is: </strong>' +
-            dayData.main.temp + '<strong><br>Min Temp: </strong>' + dayData.main.temp_min + '<strong><br>Max Temp: </strong>' + dayData.main.temp_max;
-            
-            forecastContainer.appendChild(createElement);
-            // forecast.innerHTML = JSON.stringify(data);
-        }
-    })
-    .catch(function(error) {
-        console.error('There was an issue with the fetch.', error);
-    })
+// function getFiveDayForecast(city) {
+//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=1d59d123b21d759ac3f9305f57e76274&units=imperial")
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok!');
+//             }
+//             return response.json();
+//         })
+//         .then(function (data) {
+
+//         });
+// }
+
+
+function weatherStats(city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1d59d123b21d759ac3f9305f57e76274&units=imperial")
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network not responding');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            forecastContainer.innerHTML = '';
+
+            weatherBox.appendChild(createElement);
+
+
+            const createCard = document.createElement('div');
+            createCard.classList.add('todaysWeather');
+
+            const createHeader = document.createElement('h2');
+            createHeader.textContent = city;
+
+            const createP1 = document.createElement('p');
+            createP1.textContent = "<strong><br> Current Weather: </strong>" + data.weather[0].main;
+
+            const createP2 = document.createElement('p');
+            createP2.textContent = "<strong><br>The temperature is: </strong>" + data.main.temp;
+
+            const createP3 = document.createElement('p');
+            createP3.textContent = "<strong><br>Min Temp: </strong>" + data.main.temp_min;
+
+            const createP4 = document.createElement('p');
+            createP4.textContent = "<strong><br>Max Temp: </strong>" + data.main.temp_max;
+
+            // const createP5 = document.createElement('p');
+            // createP5.textContent = ;
+
+
+
+            createCard.appendChild(createHeader);
+            createCard.appendChild(createP1);
+            createCard.appendChild(createP2);
+            createCard.appendChild(createP3);
+            createCard.appendChild(createP4);
+            // createCard.appendChild(createP5);
+
+            weatherBox.appendChild(createCard);
+
+        })
+        .catch(function (error) {
+            console.error('There was an issue with the fetch.', error);
+        });
 }
-weatherStats();
+// weatherStats(city);
 
 
 function searchWeather() {
-    const city = searchCity.value.trim();
+    const cityInput = document.querySelector('.searchCity input')
+    const city = cityInput.value.trim();
+
     if (city) {
         weatherStats(city);
     } else {
